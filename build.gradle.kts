be3786b11aaf022d14a7e0ca689e28785f75052d
Frithjof Siebecke <fsiebecke@gmail.com>
2024-11-12 19:33:30 +0100

    Build Umgebung optimiert

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val h2_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val ktor_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -32,7 +33,6 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0") // Für LocalDateTime
 
     // client
     implementation("io.ktor:ktor-client-core:$ktor_version")
@@ -41,18 +41,18 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version") // Exposed Core
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version") // Exposed DAO
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version") // Exposed JDBC
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0") // Für LocalDateTime
+
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.7") // Achte darauf, die neueste Version zu verwenden
 
-    implementation("org.jetbrains.exposed:exposed-core:0.53.0") // Exposed Core
-    implementation("org.jetbrains.exposed:exposed-dao:0.53.0") // Exposed DAO
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.53.0") // Exposed JDBC
-    implementation("org.jetbrains.exposed:exposed-java-time:0.53.0")
-
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version") // Ktor Test
-    testImplementation("io.ktor:ktor-client-core:$ktor_version") // Ktor Client
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version") // Kotlin Test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    testImplementation("io.ktor:ktor-client-core:$ktor_version") // Ktor Client
 }
 
 tasks.withType<Test> {
